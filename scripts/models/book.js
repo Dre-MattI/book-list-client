445aafed7f881f548f06c7333ee6eb34d96ecf7a
Matt <wrestler.411@comcast.net>
2017-11-09 10:44:16 -0800

    trying to add fetchOne and button formsfunctionality and button forms

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -1,17 +1,28 @@
 'use strict';
 var app = app || {};
+const __API_URL__ = 'https://mi-ai-booklist.herokuapp.com/';
+// const __API_URL__ = 'https://localhost:3000/';
 
 ((module) => {
+  Book.all = [];
+
+  function errorCallback(err) {
+    console.error(err);
+    module.errorView.initErrorPage(err);
+  }
+
   function Book(rawDataObj) {
     Object.keys(rawDataObj).forEach(key => this[key] = rawDataObj[key]);
   }
-  var __API_URL__ = 'https://mi-ai-booklist.herokuapp.com';
-  // 'http://localhost:3000';
-  Book.all = [];
 
   Book.prototype.toHtml = function() {
-    var template = Handlebars.compile($('#book-template').text());
+    var template = Handlebars.compile($('#list-template').text());
     return template(this);
+  };
+
+  Book.addDescription = function(data) {
+    var template = Handlebars.compile($('#book-display-template').text());
+    return template(data);
   };
 
   Book.loadAll = rawData => {
@@ -19,11 +30,26 @@ var app = app || {};
   };
 
   Book.fetchAll = callback => {
-    $.get(`${__API_URL__}/api/v1/books`)
-      .then(results => {
-        Book.loadAll(results);
-        callback();
+    $.get(`${__API_URL__}api/v1/books`)
+      .then(Book.loadAll)
+      .then(callback)
+      .catch(errorCallback)
+  }
+
+  Book.fetchOne = id => {
+    $.get(`${__API_URL__}book/${id}`)
+      .then(data => {
+        console.log(data);
+        $('#display').empty();
+        $('#display').append(Book.addDescription(data[0]));
       })
   };
+
+  Book.prototype.insertRecord = function(callback) {
+    $.post(`${__API_URL__}newbook/`, {title: this.title, author: this.author, isbn: this.isbn, image_url: this.image_url, description: this.description})
+      .then(console.log)
+      .then(callback)
+  };
+
   module.Book = Book;
 })(app);
